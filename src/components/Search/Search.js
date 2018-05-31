6c611f40d172843352e2e1c1cf31e5d8460c4e55
Tarang Hirani <tarang9211@gmail.com>
2018-06-01 00:25:18 +0530

    feat: implementing package search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import { Container, Columns, Column, Input } from "bloomer";
+import { Container, Columns, Column, Input, Button } from "bloomer";
 import "./Search.css";
 
 class Search extends Component {
-	constructor(props) {
-		super(props);
-	}
+	state = {
+		packageName: ""
+	};
+
+	handleChange = e => {
+		this.setState({ packageName: e.target.value });
+	};
+
+	handleClick = e => {
+		e.preventDefault();
+	};
 
 	render() {
+		const { packageName } = this.state;
 		return (
 			<Container className="margin-top-100">
 				<Columns isCentered>
@@ -15,7 +24,18 @@ class Search extends Component {
 						<label htmlFor="package-name" id="packageNameLabel">
 							Search for a Package
 						</label>
-						<Input medium className="input" id="packageName" />
+						<Input
+							medium
+							className="input"
+							id="packageName"
+							onChange={this.handleChange}
+							value={packageName}
+						/>
+					</Column>
+					<Column isSize="1/3">
+						<Button isOutlined isColor="info" onClick={this.handleClick}>
+							Search
+						</Button>
 					</Column>
 				</Columns>
 			</Container>
